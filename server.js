c7151139fc097f5c3397b7444956afa70c1b50e6
Kritika888 <kritika.srivastava888@gmail.com>
2026-05-15 16:45:25 +0530

    Api using express in mongodb

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./models/users')
+
+const app = express();
+app.use(express.json());
+
+//connect to mongodb
+mongoose.connect('mongodb://localhost:27017/kritika888')
+    .then(()=>{
+        console.log("Mongodb connected!");
+    })
+    .catch((err)=>{
+        console.log("Connection error: ", err);
+    });
+
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+
+app.post("/users", async (req, res)=>{
+    const user = await User.create(req.body);
+    res.json(user);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on 3000");
+})
