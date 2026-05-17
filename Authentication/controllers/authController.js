c187fb7dedb00e4b3029c478f26f0789b34397db
Kritika888 <kritika.srivastava888@gmail.com>
2026-05-17 11:44:33 +0530

    Authentication- Register and Login using JWT

---
+++ Authentication/controllers/authController.js
@@ -0,0 +1,67 @@
+const User = require('../../models/users');
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken")
+
+const register = async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+        const existingUser = await User.findOne ({email});
+        if(existingUser) {
+            return res.status(400).json({message: "User already exists!"});
+        }
+        const hashPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            name, email, password: hashPassword
+        });
+        res.status(201).json({
+            message: "User registered successfully!",
+            user
+        })
+    } catch (err) {
+        res.status(500).json({
+            message: err.message
+        });
+    }
+
+}
+
+const login = async (req, res) =>{
+    try{
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+        if(!user)
+        {
+            return res.status(400).json({message: "User not found!"});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch)
+        {
+            return res.status(400).json({message: "Invalid credentials!"});
+        }
+
+        //jwt token
+        const token = jwt.sign(
+            {
+                id: user._id
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "1d"
+            }
+        );
+
+        res.status(200).json({
+            message: "Login successful!",
+            token
+        });
+
+    }catch(err){
+        res.status(500).json({message: err.message});
+    }
+}
+
+module.exports = {
+    register,
+    login
+};
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const User = require('./models/users')
+const User = require('./models/users');
+const authRoutes = require('./Authentication/routes/authRoutes');
 
 const app = express();
 app.use(express.json());
@@ -18,10 +20,12 @@ app.get("/", (req, res) => {
   res.send("API running");
 });
 
-app.post("/users", async (req, res)=>{
-    const user = await User.create(req.body);
-    res.json(user);
-});
+// app.post("/users", async (req, res)=>{
+//     const user = await User.create(req.body);
+//     res.json(user);
+// });
+app.use('/auth', authRoutes);
+
 
 app.listen(3000, () => {
   console.log("Server running on 3000");
